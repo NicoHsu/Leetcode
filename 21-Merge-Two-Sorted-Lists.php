b81a88967a2af9b430771b854da783f53bd0a9ad
Nico <nicohsu000@gmail.com>
2021-03-13 16:58:48 +0800

    feat(easy): 21. Merge Two Sorted Lists with recursion and iteration

--- 21-Merge-Two-Sorted-Lists.php
+++ 21-Merge-Two-Sorted-Lists.php
@@ -1,6 +1,6 @@
 <?php
 
-// Easy (using recursion)
+// Easy
 /**
  * Definition for a singly-linked list.
  */
@@ -16,11 +16,12 @@ class ListNode {
 class Solution {
 
     /**
+     * using recursion
      * @param ListNode $l1
      * @param ListNode $l2
      * @return ListNode
      */
-    function mergeTwoLists($l1, $l2) {
+    function mergeTwoListsRecursion($l1, $l2) {
         if(is_null($l1)) return $l2;
         if(is_null($l2)) return $l1;
         if ($l1->val < $l2->val) {
@@ -31,6 +32,34 @@ class Solution {
             return $l2;
         }
     }
+
+    /**
+     * using iteration
+     * @param ListNode $l1
+     * @param ListNode $l2
+     * @return ListNode
+     */
+    function mergeTwoListsIteration($l1, $l2) {
+        $node = $handler = new ListNode();
+        while(!is_null($l1) && !is_null($l2)) {
+            if ($l1->val < $l2->val) {
+                $handler->next = $l1;
+                $l1 = $l1->next;
+            } else {
+                $handler->next = $l2;
+                $l2 = $l2->next;
+            }
+            $handler = $handler->next;
+        }
+
+        if(is_null($l1)) {
+            $handler->next = $l2;
+        } else if(is_null($l2)) {
+            $handler->next = $l1;
+        }
+
+        return $node->next;
+    }
 }
 
 // Input: l1 = [1,2,4], l2 = [1,3,4]
@@ -40,4 +69,4 @@ $l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 $l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
 $s = new Solution();
-var_dump($s->mergeTwoLists($l1, $l2));
+var_dump($s->mergeTwoListsIteration($l1, $l2));
